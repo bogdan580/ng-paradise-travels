047a07b39b5ebdff235e4afe9fdae3ba5450e135
Mykola Bohdan <bogdan.nick007@gmail.com>
2018-06-29 15:25:24 +0200

    poprawione styli i struktura aplikacji

---
+++ src/app/system/offer-page/offer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wfm-offer-page',
+  templateUrl: './offer-page.component.html',
+  styleUrls: ['./offer-page.component.css']
+})
+export class OfferPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -4,10 +4,11 @@ import {SharedModule} from '../shared/shared.module';
 import {SystemRoutingModule} from './system-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { OffersPageComponent } from './offers-page/offers-page.component';
+import { OfferPageComponent } from './offer-page/offer-page.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, SystemRoutingModule],
   exports: [CommonModule, SharedModule],
-  declarations: [HomePageComponent, OffersPageComponent]
+  declarations: [HomePageComponent, OffersPageComponent, OfferPageComponent]
 })
 export class SystemModule {}
