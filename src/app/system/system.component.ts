d61b851e20441cc51f635aaedf94aac33feb2329
AyeSirr <przemekkosno5@gmail.com>
2018-07-06 02:41:19 +0200

    Znikanie panelu admina

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -5,19 +5,32 @@ import {UsersService} from '../shared/services/users.service';
 import {PojoBooleanModel} from '../shared/models/pojoModels/pojoBoolean.model';
 import {Message} from '../shared/models/message.model';
 import {AuthService} from '../shared/services/auth.service';
+import {User} from '../shared/models/user.model';
 
 @Component({
   selector: 'wfm-system',
   templateUrl: './system.component.html'
 })
 export class SystemComponent implements OnInit {
+  user: User;
+  isAdm: boolean;
   constructor (
     private router: Router,
     private usersService: UsersService,
-    private authService: AuthService) {}
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.router.navigate(['/system/home']);
+    this.usersService.getLoggedUser()
+      .subscribe((user: User) => {
+        console.log(user);
+        console.log('xxxx');
+        this.user = user;
+        this.Check(this.user.role);
+        console.log(this.user.role);
+        console.log(this.isAdm);
+      });
   }
 
   onClickLogout() {
@@ -25,8 +38,17 @@ export class SystemComponent implements OnInit {
       .subscribe((pojoBolleanModel: PojoBooleanModel) => {
         if (pojoBolleanModel.value === true) {
           this.authService.logout();
+          this.isAdm = false;
           this.router.navigate(['/home']);
         }
       });
+  }
+  Check(role: string): boolean {
+    if (role === 'admin') {
+      this.isAdm = true;
+    } else {
+      this.isAdm = false;
+    }
+    return this.isAdm;
   }
 }
