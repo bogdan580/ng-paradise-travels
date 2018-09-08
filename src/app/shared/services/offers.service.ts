7787bd551c2f1b2beeb7e8bbfafb6c13db5b31ef
AyeSirr <przemekkosno5@gmail.com>
2018-09-08 13:38:14 +0200

    Search for offers

--- src/app/shared/services/offers.service.ts
+++ src/app/shared/services/offers.service.ts
@@ -31,7 +31,26 @@ export class OffersService {
   getWeather(city: string): Observable<any> {
     return this.http.get<any>(this.configService.getWeatherUrl() + 'q=' + city + '&units=metric&appid=10c30b170fc663080002c992eca0407b');
   }
-
+  getSearch(location: string, datefrom: Date, dateto: Date, pricefrom: number, priceto: number): Observable<any> {
+    let search = '/offers/search?';
+    if (datefrom) {
+      search = search + '&dateFrom=' + datefrom;
+    }
+    if (dateto) {
+      search = search + '&dateTo=' + dateto;
+    }
+    if (location) {
+      search = search + '&location=' + location;
+    }
+    if (pricefrom) {
+      search = search + '&priceFrom=' + pricefrom;
+    }
+    if (priceto) {
+      search = search + '&priceTo=' + priceto;
+    }
+    console.log(search);
+    return this.http.get<any>(this.configService.getBeckendUrl() + search);
+  }
   buy(offerBuyRequestModel: OfferBuyRequestModel): Observable<PojoBooleanModel> {
     return this.http.post<PojoBooleanModel>( this.configService.getBeckendUrl() + '/offers/buy',
       offerBuyRequestModel
--- src/app/system/offers-page/offers-page.component.ts
+++ src/app/system/offers-page/offers-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {OffersService} from '../../shared/services/offers.service';
 import {Offer, Convert} from '../../shared/models/offer.model';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
 @Component({
   selector: 'wfm-offers-page',
   templateUrl: './offers-page.component.html',
@@ -8,16 +10,35 @@ import {Offer, Convert} from '../../shared/models/offer.model';
 })
 export class OffersPageComponent implements OnInit {
   offers: Offer;
-  constructor(private offersService: OffersService) { }
-
+  myfind: Array<any>;
+  search: boolean;
+  constructor(private offersService: OffersService, private router: Router) { }
+  formS: FormGroup;
   ngOnInit() {
    this.getOffers();
+    this.search = false;
+    this.formS = new FormGroup({
+      'location': new FormControl(),
+      'datefrom': new FormControl(),
+      'dateto': new FormControl(),
+      'pricefrom': new FormControl(),
+      'priceto': new FormControl()
+    });
   }
   getOffers(): void {
     this.offersService.getOffers().subscribe(oferta => {
       this.offers = oferta;
     });
   }
-
+  onSubmitSearch() {
+    const {location, datefrom, dateto,  pricefrom, priceto} = this.formS.value;
+    console.log(this.formS.value);
+    console.log(location);
+    this.offersService.getSearch(location, datefrom, dateto, pricefrom, priceto).subscribe(research => {
+      this.myfind = research;
+      this.search = true;
+    console.log(this.myfind); }
+    );
+  }
 
 }
